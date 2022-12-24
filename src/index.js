10f9c675100636b6996aaed94be846da0b382fb4
Raul <raulvillajr95@gmail.com>
2022-12-23 20:20:10 -0800

    Complete basic webpack setup

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import './style.css';
+
+console.log('toto')
+
+/**
+ * add everything to github
+ * set up image with webpack
+ * set up scripts like watch, etc.
+ * 
+ * later:
+ * remove dev mode
+ */
