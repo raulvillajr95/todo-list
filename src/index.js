96c8bcf9e9826b06f053fe744aae0806faa52bf6
Raul <raulvillajr95@gmail.com>
2022-12-23 21:51:44 -0800

    pre planning and understanding requirements

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 import './style.css';
 
+let note = {
+  title: 'Milk',
+  description: 'I need 2 gallons 2% milk asap for cereal',
+  dueDate: '',
+}
+
 console.log('toto')
 
 /**
- * add everything to github
- * set up image with webpack
- * set up scripts like watch, etc.
+ * start by making todo list work all on console
+ * plan out ui/ux
+ *  it's my first time pre-planning
+ *    so only use whimsical
+ * 
+ * notes:
+ * -todos need to be objects, kinda like the 'books'
+ *  preset for library
+ * -each 'todo' has a 'title'(string), 'description'(string),
+ *  'dueData'(maybe a Date-fns), 'priority'(maybe a number?)
+ * -have folders of 'todo's
+ * -have a module file for each thing
+ * -ui should be able to:
+ *    view of multiple folders
+ *    view of multiple 'todo's
+ *    view 1 full todo
+ *    delete todo
+ *    delete folder
+ * -modeled liked iOS Reminders but simpler
+ * -use date-fns library
+ * -use localStorage to save to local browser
  * 
  * later:
+ * add eslint and prettier
+ * improve it UI/UX wise
  * remove dev mode
+ * do 10 exercises in each, Exercism, CodeWars, & HackerRank
  */
