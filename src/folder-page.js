e092c05667229e2ea91436e92c198e0a92be50ee
Raul <raulvillajr95@gmail.com>
2023-01-04 15:38:31 -0800

    create folderPage function

---
+++ src/folder-page.js
@@ -0,0 +1,38 @@
+import { displayTitle, displayTodoForm, clearDisplay } from './helpers';
+import { listOfTodosToDisplay } from './folder-helpers';
+import { createTodo } from './todo-helpers';
+import todoDependencies from './dependencies';
+
+// Folder page,(manual name for now)
+function folderPage(folder) {
+  displayTitle('folder', folder);
+  displayTodoForm('folder');
+  listOfTodosToDisplay(folder);
+
+  // Create todo
+  const todoAddFolderBtn = document.querySelector('#todo-add-folder-btn');
+  todoAddFolderBtn.addEventListener('click', () => {
+    const todoTitleFolder = document.querySelector('#todo-title-folder');
+    const todoDuedateFolder = document.querySelector('#todo-duedate-folder');
+    const todoPriorityFolder = document.querySelector('#todo-priority-folder');
+    const todoDescriptionFolder = document.querySelector(
+      '#todo-description-folder'
+    );
+    // Manual for now
+    createTodo(
+      todoTitleFolder.value,
+      todoDescriptionFolder.value,
+      todoDuedateFolder.value,
+      todoPriorityFolder.value,
+      'Default'
+    );
+    clearDisplay('#list-folder');
+    listOfTodosToDisplay('Default');
+    todoTitleFolder.value = '';
+    todoDuedateFolder.value = '';
+    todoPriorityFolder.value = '';
+    todoDescriptionFolder.value = '';
+  });
+}
+
+export default folderPage;
--- src/index.js
+++ src/index.js
@@ -2,48 +2,46 @@ import './style.css';
 import './reset.css';
 import todoDependencies from './dependencies';
 // import homePage from './home-page';
-import { displayTitle, displayTodoForm, clearDisplay } from './helpers';
+import { clearDisplay } from './helpers';
 import { createTodo } from './todo-helpers';
 import { listOfTodosToDisplay } from './folder-helpers';
+import folderPage from './folder-page';
 
 // Home Page
 // homePage();
 
 // Folder page,(manual name for now)
-// Maybe make these 3 lines a function
-displayTitle('folder', 'Default');
-displayTodoForm('folder');
-listOfTodosToDisplay('Default');
+folderPage('Default');
 
 // Create todo
 // Either make whole piece a function or add to Folder Page
-const todoAddFolderBtn = document.querySelector('#todo-add-folder-btn');
-todoAddFolderBtn.addEventListener('click', () => {
-  const todoTitleFolder = document.querySelector('#todo-title-folder');
-  const todoDuedateFolder = document.querySelector('#todo-duedate-folder');
-  const todoPriorityFolder = document.querySelector('#todo-priority-folder');
-  const todoDescriptionFolder = document.querySelector(
-    '#todo-description-folder'
-  );
-  // Manu
-  createTodo(
-    todoTitleFolder.value,
-    todoDescriptionFolder.value,
-    todoDuedateFolder.value,
-    todoPriorityFolder.value,
-    'Default'
-  );
-  clearDisplay('#list-folder');
-  listOfTodosToDisplay('Default');
-  todoTitleFolder.value = '';
-  todoDuedateFolder.value = '';
-  todoPriorityFolder.value = '';
-  todoDescriptionFolder.value = '';
+// const todoAddFolderBtn = document.querySelector('#todo-add-folder-btn');
+// todoAddFolderBtn.addEventListener('click', () => {
+//   const todoTitleFolder = document.querySelector('#todo-title-folder');
+//   const todoDuedateFolder = document.querySelector('#todo-duedate-folder');
+//   const todoPriorityFolder = document.querySelector('#todo-priority-folder');
+//   const todoDescriptionFolder = document.querySelector(
+//     '#todo-description-folder'
+//   );
+//   // Manu
+//   createTodo(
+//     todoTitleFolder.value,
+//     todoDescriptionFolder.value,
+//     todoDuedateFolder.value,
+//     todoPriorityFolder.value,
+//     'Default'
+//   );
+//   clearDisplay('#list-folder');
+//   listOfTodosToDisplay('Default');
+//   todoTitleFolder.value = '';
+//   todoDuedateFolder.value = '';
+//   todoPriorityFolder.value = '';
+//   todoDescriptionFolder.value = '';
 
-  // Test logs
-  console.log(todoDependencies.defaultFolder, 'default folders');
-  console.log(todoDependencies.folders, 'folders');
-});
+//   // Test logs
+//   console.log(todoDependencies.defaultFolder, 'default folders');
+//   console.log(todoDependencies.folders, 'folders');
+// });
 
 // Just test logging results
 console.log(todoDependencies.defaultFolder, 'default folders');
